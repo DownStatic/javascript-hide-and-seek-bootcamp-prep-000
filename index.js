1fd4c638e487976412925668ad9853966f979d1b
Christopher Stull <chris.stull@gmail.com>
2018-05-26 02:10:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested').querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var lists = document.querySelectorAll(".ranked-list")
+  for (let i= 0; i<lists.length;i++){
+    lists[i].innerHTML = parseInt(n) + parseInt(lists[i].innerHTML)
+  }
+}
+
+function deepestChild(){
+  var grand = document.querySelector('#grand-node')
+  while (typeof(grand.children[0]) != 'undefined') {
+    grand = grand.children[0]
+  }
+  return grand
+}
